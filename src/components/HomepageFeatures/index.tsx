35c90ac1e7c02814c10de453e8569bffc8997844
Todd Baert <toddbaert@gmail.com>
2022-07-05 14:07:40 -0400

    Add code-blocks
    Signed-off-by: Todd Baert <toddbaert@gmail.com>

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -2,6 +2,9 @@
 import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
+import CodeBlock from '@theme/CodeBlock';
+import Tabs from '@theme/Tabs';
+import TabItem from '@theme/TabItem';
 
 type FeatureItem = {
   title: string;
@@ -11,43 +14,165 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Vendor neutrality',
+    title: 'Simple API',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and used to get your website up and running
-        quickly.
-      </>
+      <Tabs>
+        <TabItem value="ts" label="TypeScript">
+          <div>
+            {/* prettier-ignore */}
+            <CodeBlock className="language-ts">{`
+const value = client
+  .getBooleanValue('new-look', false );
+            `}</CodeBlock>
+          </div>
+        </TabItem>
+        <TabItem value="java" label="Java">
+          {/* prettier-ignore */}
+          <CodeBlock className="language-java">{`
+Boolean value = client
+  .getBooleanValue("new-look", false);
+          `}</CodeBlock>
+        </TabItem>
+        <TabItem value="go" label="Go">
+          {/* prettier-ignore */}
+          <CodeBlock className="language-go">{`
+value, err := client
+  .GetBooleanValue("new-look", true, nil)
+          `}</CodeBlock>
+        </TabItem>
+      </Tabs>
     ),
   },
   {
-    title: 'Extensibility',
+    title: 'Easy integration',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go ahead and move your docs into the{' '}
-        <code>docs</code> directory.
-      </>
+      <Tabs>
+        <TabItem value="ts" label="TypeScript">
+          <div>
+            {/* prettier-ignore */}
+            <CodeBlock className="language-ts">{`
+class MyFlagProvider implements Provider {
+  //...
+  resolveBooleanEvaluation(
+    flagKey: string,
+    defaultValue: boolean,
+    context: Context,
+    options: FlagEvaluationOptions | undefined
+  ): Promise<ResolutionDetails<boolean>> {
+    // your implementation
+  }
+  //...
+}
+            `}</CodeBlock>
+          </div>
+        </TabItem>
+        <TabItem value="java" label="Java">
+          {/* prettier-ignore */}
+          <CodeBlock className="language-java">{`
+class MyFlagProvider implements Provider {
+  //...
+  @Override
+  public ProviderEvaluation<Boolean> getBooleanEvaluation(
+    String flagKey,
+    Boolean defaultValue,
+    EvaluationContext ctx,
+    FlagEvaluationOptions options) {
+      // your implementation
+  }
+  //...
+}
+          `}</CodeBlock>
+        </TabItem>
+        <TabItem value="go" label="Go">
+          {/* prettier-ignore */}
+          <CodeBlock className="language-go">{`
+type MyFlagProvider struct {}
+//...
+func (p MyFlagProvider) GetBooleanEvaluation(
+  flag string,
+  defaultValue bool,
+  evalCtx EvaluationContext,
+  options ...EvaluationOption) BoolResolutionDetail {
+    // your implementation
+}
+//...
+          `}</CodeBlock>
+        </TabItem>
+      </Tabs>
     ),
   },
   {
-    title: 'Community',
+    title: 'Powerful extensions',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can be extended while reusing the same
-        header and footer.
-      </>
-    ),
-  },
-  {
-    title: 'Education',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can be extended while reusing the same
-        header and footer.
-      </>
+      <Tabs>
+        <TabItem value="ts" label="TypeScript">
+          <div>
+            {/* prettier-ignore */}
+            <CodeBlock className="language-ts">{`
+class MyHook implements Hook {
+  before(
+    hookContext: BeforeHookContext,
+    hookHints?: HookHints) {
+    // do something before flag evaluation
+  }
+
+  after(
+    hookContext: Readonly<HookContext<boolean>>,
+    evaluationDetails: EvaluationDetails<boolean>,
+    hookHints?: HookHints
+  ) {
+    // do something after flag evaluation
+  }
+}
+            `}</CodeBlock>
+          </div>
+        </TabItem>
+        <TabItem value="java" label="Java">
+          {/* prettier-ignore */}
+          <CodeBlock className="language-java">{`
+class MyHook implements Hook {
+  Optional<EvaluationContext> before(
+    HookContext<T> ctx,
+    Map<String, Object>
+    hints) {
+    // do something before flag evaluation
+  }
+
+  void after(
+    HookContext<T> ctx,
+    FlagEvaluationDetails<T> details,
+    Map<String,
+    Object> hints) {
+    // do something after flag evaluation
+  }
+}
+          `}</CodeBlock>
+        </TabItem>
+        <TabItem value="go" label="Go">
+          {/* prettier-ignore */}
+          <CodeBlock className="language-go">{`
+type MyHook struct {}
+//...
+func (h MyHook) Before(
+  ctx HookContext,
+  details FlagEvaluationDetails,
+  hints interface{}) error {
+    // do something before flag evaluation
+}
+
+func (h MyHook) After(
+  ctx HookContext,
+  details FlagEvaluationDetails,
+  hints interface{}) (EvaluationContext, error) {
+    // do something after flag evaluation
+}
+//...
+          `}</CodeBlock>
+        </TabItem>
+      </Tabs>
     ),
   },
 ];
