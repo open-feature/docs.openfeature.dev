9330e9165a786f2a162f9991c28bd30ff5fd4104
Michael Beemer <michael.beemer@dynatrace.com>
2022-07-18 12:03:19 -0400

    fix: update edit urls to use the correct repo
    Signed-off-by: Michael Beemer <michael.beemer@dynatrace.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -31,15 +31,13 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/open-feature/docs.openfeature.dev/edit/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/open-feature/docs.openfeature.dev/edit/main/',
         },
         theme: {
           customCss: [require.resolve('./src/css/custom.css'), require.resolve('@fortawesome/fontawesome-svg-core/styles.css')],
