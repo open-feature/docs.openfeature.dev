a64f80843b4648045fe754c37768d6bb275d0adc
Michael Beemer <michael.beemer@dynatrace.com>
2022-07-29 12:03:14 -0400

    Remove git-dir from submodule plugin
    Signed-off-by: Michael Beemer <michael.beemer@dynatrace.com>

--- src/plugins/update-git-submodule.js
+++ src/plugins/update-git-submodule.js
@@ -1,13 +1,11 @@
 const util = require('node:util');
 const exec = util.promisify(require('node:child_process').exec);
 
-module.exports = async function updateSubmodulePlugin(context) {
-  try {
-    console.log('attempting to update the git submodules');
-    await exec(`git --git-dir ${context.siteDir}/.git submodule update --init --recursive`);
-  } catch (err) {
-    console.err(err);
-    throw new Error('Unable to update the required git submodules');
+module.exports = async function updateSubmodulePlugin() {
+  console.log('attempting to update the git submodules');
+  const { stderr } = await exec(`git submodule update --init --recursive`);
+  if (stderr) {
+    throw new Error(`Error pulling submodule: ${stderr}`);
   }
 
   return {
