8ec97eacae674d03fa5044ce365ef232ae7b087e
Michael Beemer <beeme1mr@users.noreply.github.com>
2022-09-07 13:07:35 -0400

    add split java provider
    Signed-off-by: Michael Beemer <beeme1mr@users.noreply.github.com>

--- src/components/TechnologiesFeatures/JavaFeatures/index.tsx
+++ src/components/TechnologiesFeatures/JavaFeatures/index.tsx
@@ -2,38 +2,46 @@ import { faJava } from '@fortawesome/free-brands-svg-icons';
 import React from 'react';
 import { OpenFeatureTechnologiesPage } from '../../custom/OpenFeatureTechnologiesPage';
 import FlagdSvg from '@site/static/img/flagd-no-fill.svg';
+import SplitSvg from '@site/static/img/split-no-fill.svg';
 
-export class JavaFeatures
- extends React.Component {
+export class JavaFeatures extends React.Component {
   override render() {
-    return <OpenFeatureTechnologiesPage 
-      technology="Java"
-      iconDefinition={ faJava }
-      sdkRepoLink={{
-        title: 'Github repository',
-        href: 'https://github.com/open-feature/java-sdk'
-      }}
-      artifact={{
-        title: 'Maven Central repository',
-        href: 'https://search.maven.org/artifact/dev.openfeature/javasdk/',
-        instruction: `
+    return (
+      <OpenFeatureTechnologiesPage
+        technology="Java"
+        iconDefinition={faJava}
+        sdkRepoLink={{
+          title: 'Github repository',
+          href: 'https://github.com/open-feature/java-sdk',
+        }}
+        artifact={{
+          title: 'Maven Central repository',
+          href: 'https://search.maven.org/artifact/dev.openfeature/javasdk/',
+          instruction: `
 <dependency>
   <groupId>dev.openfeature</groupId>
   <artifactId>javasdk</artifactId>
   <version>\${openfeature.version}</version>
 </dependency>
         `,
-        codeBlockLanguage: 'xml'
-      }}
-      providers={[
-        {
-          title: 'flagd Provider',
-          description: 'A provider for flagd',
-          href: 'https://github.com/open-feature/java-sdk-contrib/tree/main/providers/flagd',
-          svg: FlagdSvg
-        }
-      ]}
-      hooks={[]}
-      />;
+          codeBlockLanguage: 'xml',
+        }}
+        providers={[
+          {
+            title: 'flagd Provider',
+            description: 'A provider for flagd',
+            href: 'https://github.com/open-feature/java-sdk-contrib/tree/main/providers/flagd',
+            svg: FlagdSvg,
+          },
+          {
+            title: 'Split Provider',
+            description: 'The official Split provider for OpenFeature',
+            href: 'https://github.com/splitio/split-openfeature-provider-java',
+            svg: SplitSvg,
+          },
+        ]}
+        hooks={[]}
+      />
+    );
   }
 }
