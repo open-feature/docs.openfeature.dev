51761f104641227269b027ea431b03cc8139f3f1
Todd Baert <toddbaert@gmail.com>
2022-12-02 08:54:36 -0500

    doc: add dotnet goff (#203)
    Signed-off-by: Todd Baert <toddbaert@gmail.com>
    
    Signed-off-by: Todd Baert <toddbaert@gmail.com>

--- src/components/ServerTechnologiesFeatures/DotnetFeatures/index.tsx
+++ src/components/ServerTechnologiesFeatures/DotnetFeatures/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { OpenFeatureTechnologiesPage } from '../../custom/OpenFeatureTechnologiesPage';
 import CSharpNoFillSvg from '@site/static/img/c-sharp-no-fill.svg';
 import CloudbeesSvg from '@site/static/img/cloudbees-no-fill.svg';
+import GoFeatureFlagNoFillSvg from '@site/static/img/goff-no-fill.svg';
 import LaunchDarklySvg from '@site/static/img/launchdarkly-no-fill.svg';
 import SplitSvg from '@site/static/img/split-no-fill.svg';
+import React from 'react';
+import { OpenFeatureTechnologiesPage } from '../../custom/OpenFeatureTechnologiesPage';
 
 export class DotnetFeatures extends React.Component {
   override render() {
@@ -28,6 +29,12 @@ export class DotnetFeatures extends React.Component {
             href: 'https://github.com/rollout/cloudbees-openfeature-provider-dotnet',
             svg: CloudbeesSvg,
             vendorOfficial: true
+          },
+          {
+            title: 'GO Feature Flag Provider',
+            description: 'A provider for GO Feature Flag',
+            href: 'https://github.com/open-feature/dotnet-sdk-contrib/tree/main/src/OpenFeature.Contrib.Providers.GOFeatureFlag',
+            svg: GoFeatureFlagNoFillSvg,
           },
           {
             title: 'LaunchDarkly Provider',
