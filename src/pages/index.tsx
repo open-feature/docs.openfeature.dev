595770abd2ec873d97164aba3d1def07c02f988a
Nick Bence <nick.bence@dynatrace.com>
2023-03-31 13:54:47 -0400

    add react landing template https://github.com/cruip/open-react-template

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,52 @@
-import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import { useLocation } from '@docusaurus/router';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
-import styles from './index.module.css';
-
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to="docs/reference/intro">
-            Get started
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import React, { useEffect } from 'react';
+import '../css/style.css';
+import FeaturesBlocks from '../partials/FeaturesBlocks';
+import FeaturesZigZag from '../partials/FeaturesZigzag';
+import Footer from '../partials/Footer';
+import HeroHome from '../partials/HeroHome';
+import Newsletter from '../partials/Newsletter';
+import PageIllustration from '../partials/PageIllustration';
+import Testimonials from '../partials/Testimonials';
 
 export default function Home(): JSX.Element {
+  const location = useLocation();
+
+  useEffect(() => {
+    AOS.init({
+      once: true,
+      disable: 'phone',
+      duration: 1000,
+      easing: 'ease-out-sine',
+    });
+  });
+
+  useEffect(() => {
+    document.querySelector('html').style.scrollBehavior = 'auto';
+    window.scroll({ top: 0 });
+    document.querySelector('html').style.scrollBehavior = '';
+  }, [location.pathname]);
+
   return (
-    <Layout description="The official documentation of the OpenFeature project.">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
+    <div className="font-inter antialiased bg-gray-900 text-gray-200 tracking-tight">
+      <Layout description="The official documentation of the OpenFeature project.">
+        <div className="flex flex-col min-h-screen overflow-hidden">
+          <main className="grow">
+            <div className="relative max-w-6xl mx-auto h-0 pointer-events-none" aria-hidden="true">
+              <PageIllustration />
+            </div>
+            <HeroHome />
+            <FeaturesBlocks />
+            <FeaturesZigZag />
+            <Testimonials />
+            <Newsletter />
+          </main>
+          <Footer />
+        </div>
+      </Layout>
+    </div>
   );
 }
